35f54d93b49c4360beb3ae852a0c61d651d13e04
Linsl <2286627903@qq.com>
2022-09-28 23:11:50 +0800

    完成随机生成一个要求公式

---
+++ PSP表格.md
@@ -0,0 +1,23 @@
+
+
+## PSP表格
+
+| PSP2.1                                  | Personal Software Process Stages        | 预估耗时（分钟） | 实际耗时（分钟） |
+| --------------------------------------- | --------------------------------------- | ---------------- | ---------------- |
+| · Planning                              | · 计划                                  | 20               |                  |
+| · Estimate                              | · 估计这个任务需要多少时间              | 360              |                  |
+| · Development                           | · 开发                                  | 260              |                  |
+| · Analysis                              | · 需求分析 (包括学习新技术)             | 20               |                  |
+| · Design Spec                           | · 生成设计文档                          | 20               |                  |
+| · Design Review                         | · 设计复审 (和同事审核设计文档)         | 10               |                  |
+| · Coding Standard                       | · 代码规范 (为目前的开发制定合适的规范) | 10               |                  |
+| · Design                                | · 具体设计                              | 60               |                  |
+| · Coding                                | · 具体编码                              | 100              |                  |
+| · Code Review                           | · 代码复审                              | 20               |                  |
+| · Test                                  | · 测试（自我测试，修改代码，提交修改）  | 20               |                  |
+| · Reporting                             | · 报告                                  | 30               |                  |
+| · Test Report                           | · 测试报告                              | 30               |                  |
+| · Size Measurement                      | · 计算工作量                            |                  |                  |
+| · Postmortem & Process Improvement Plan | · 事后总结, 并提出过程改进计划          | 30               |                  |
+| 合计                                    |                                         | 450              |                  |
+
---
+++ 小学四则运算/Four Arithmetic.js
@@ -0,0 +1,207 @@
+{
+
+    /*
+    定义构造真分数的函数，规定分子分母都是正整数类型且不为0
+    */
+    function True_Faction(molecule, denominator) {
+        // molecule为分子，denominator为分母
+        // 获取又值（比如1又3分之2的1）
+        // - 获取整数
+        // - 分子改成比分母小的数
+        this.integer = parseInt(molecule / denominator);
+        molecule -= this.integer * denominator;
+
+        // 寻找公约数
+        if (molecule == 0) {
+            denominator = 1;
+        } else {
+            let flag = 1;
+            while (flag == 1) {
+                let sum;
+                for (let i = 0; i <= molecule; i++) {
+                    if (molecule % i == 0 && denominator % i == 0) {
+                        sum = i;
+                    }
+                }
+                if (sum == 1) {
+                    flag = 0;
+                } else {
+                    molecule = molecule / sum;
+                    denominator = denominator / sum;
+                }
+            }
+        }
+
+
+        this.molecule = molecule;
+        this.denominator = denominator;
+    }
+
+    /*
+    定义一个表达式数据结构的构造函数
+    */
+    function Expression(Array_ExpresstionString, answer) {
+        this.Array_ExpresstionString = Array_ExpresstionString;
+        this.answer = answer;
+    }
+
+    /*
+    为真分数的构造函数原型添加toString方法
+    */
+    True_Faction.prototype.toString = function () {
+        if (this.molecule == 0 && this.integer != 0) {
+            return this.integer;
+        } else if (this.molecule != 0 && this.integer == 0) {
+            return this.molecule + '/' + this.denominator;
+        } else if (this.molecule != 0 && this.integer != 0) {
+            return this.integer + '`' + this.molecule + '/' + this.denominator;
+        }
+    }
+
+    function Four_Airthmetic(n, r) {
+        this.n = n;
+        this.r = r;
+    }
+
+    Four_Airthmetic.prototype.getOneAirthmetic = function () {
+        // 生成数学字符的数目
+        let Number_MathSign = Math.floor(Math.random() * 3 + 1);
+        // console.log('字符数为'+Number_MathSign)
+
+        // 生成随机字符数目的队列
+        // - 0,1,2,3分别代表+，-，*，/
+        let Array_char = ['+', '-', '*', '/'];
+        let Array_MathSign = new Array(Number_MathSign);
+        for (let i = 0; i < Number_MathSign; i++) {
+            let flag = Math.floor(Math.random() * 4);
+            Array_MathSign[i] = Array_char[flag];
+            console.log(Array_MathSign[i]);
+        }
+
+        // 生成随机
+        let Array_Number = new Array(Number_MathSign + 1);
+        for (let i = 0; i < Number_MathSign + 1; i++) {
+            let flag = Math.floor(Math.random() * 2);
+            if (flag == 0) {
+                Array_Number[i] = new True_Faction(Math.floor(Math.random(this.r - 1) + 1), 1);
+            } else {
+                Array_Number[i] = new True_Faction(Math.floor(Math.random(this.r - 1) + 1), Math.floor(Math.random() * (this.r - 1) + 1));
+            }
+            console.log('this:' + Array_Number[i]);
+        }
+
+        // 随机生成括号
+        let Array_ExpresstionString = [];
+
+        Array_ExpresstionString.push(Array_Number[0]);
+        for (let i = 0; i < Number_MathSign; i++) {
+            Array_ExpresstionString.push(Array_MathSign[i]);
+            Array_ExpresstionString.push(Array_Number[i + 1]);
+        }
+        console.log(Array_ExpresstionString);
+        if (Number_MathSign == 2) {
+            let flag = Math.floor(Math.random() * 2);
+            if (flag == 0) {
+                Array_ExpresstionString.splice(2, 0, "(");
+                Array_ExpresstionString.push(')');
+                console.log('2-1');
+            }
+        } else if (Number_MathSign == 3) {
+            let flag = Math.floor(Math.random() * 3);
+            if (flag == 0) {
+                Array_ExpresstionString.splice(2, 0, '(');
+                Array_ExpresstionString.push(')');
+                console.log('3-0');
+            }
+            else if (flag == 1) {
+                Array_ExpresstionString.splice(2, 0, '(', '(');
+                Array_ExpresstionString.splice(7, 0, ')');
+                Array_ExpresstionString.push(')');
+                console.log('3-1');
+            }
+            else if (flag == 2) {
+                Array_ExpresstionString.splice(2, 0, '(');
+                Array_ExpresstionString.splice(5, 0, '(');
+                Array_ExpresstionString.push(')');
+                Array_ExpresstionString.push(')');
+                console.log('3-2');
+            }
+        }
+
+        let Array_x = [];
+        for (let i = 0; i < Array_ExpresstionString.length; i++) {
+            Array_x.push(Array_ExpresstionString[i]);
+        }
+
+        let answer = getAnswerFromExpresstion(Array_x);
+        let expression = new Expression(Array_ExpresstionString, answer);
+
+        if (answer == null) {
+            return this.getOneAirthmetic();
+        } else return expression;
+
+    }
+
+    // 创建包含括号时的表达式的算法
+    let getAnswerFromExpresstion = function (Array_ExpresstionString) {
+        for (let i = 0; i < Array_ExpresstionString.length; i++) {
+            if (Array_ExpresstionString[i] == '(') {
+                for (let j = Array_ExpresstionString.length - 1; j >= 0; j--) {
+                    if (Array_ExpresstionString[j] == ')') {
+                        let x = getAnswerFromExpresstion(Array_ExpresstionString.splice(i + 1, j - i - 1))
+                        if (x == null) {
+                            return null;
+                        }
+                        Array_ExpresstionString.splice(2, 2, x);
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < Array_ExpresstionString.length; i++) {
+            if (Array_ExpresstionString[i] == '*') {
+                let a = (Array_ExpresstionString[i - 1].integer * Array_ExpresstionString[i - 1].denominator + Array_ExpresstionString[i - 1].molecule) * (Array_ExpresstionString[i + 1].integer * Array_ExpresstionString[i + 1].denominator + Array_ExpresstionString[i + 1].molecule);
+                let b = (Array_ExpresstionString[i - 1].denominator) * (Array_ExpresstionString[i + 1].denominator);
+                let newnumber = new True_Faction(a, b);
+                Array_ExpresstionString.splice(i - 1, 3, newnumber);
+            } else if (Array_ExpresstionString[i] == '/') {
+                let a = (Array_ExpresstionString[i - 1].integer * Array_ExpresstionString[i - 1].denominator + Array_ExpresstionString[i - 1].molecule) * (Array_ExpresstionString[i + 1].denominator);
+                let b = (Array_ExpresstionString[i - 1].denominator) * (Array_ExpresstionString[i + 1].integer * Array_ExpresstionString[i + 1].denominator + Array_ExpresstionString[i + 1].molecule);
+                let newnumber = new True_Faction(a, b);
+                Array_ExpresstionString.splice(i - 1, 3, newnumber);
+            }
+        }
+        for (let i = 0; i < Array_ExpresstionString.length; i++) {
+            if (Array_ExpresstionString[i] == '+') {
+                let a = ((Array_ExpresstionString[i - 1].integer * Array_ExpresstionString[i - 1].denominator + Array_ExpresstionString[i - 1].molecule) * (Array_ExpresstionString[i + 1].denominator)) + ((Array_ExpresstionString[i + 1].integer * Array_ExpresstionString[i + 1].denominator + Array_ExpresstionString[i + 1].molecule) * (Array_ExpresstionString[i - 1].denominator));
+                let b = (Array_ExpresstionString[i - 1].denominator) * (Array_ExpresstionString[i + 1].denominator);
+                let newnumber = new True_Faction(a, b);
+                Array_ExpresstionString.splice(i - 1, 3, newnumber);
+            } else if (Array_ExpresstionString[i] == '/') {
+                let a = ((Array_ExpresstionString[i - 1].integer * Array_ExpresstionString[i - 1].denominator + Array_ExpresstionString[i - 1].molecule) * (Array_ExpresstionString[i + 1].denominator)) - ((Array_ExpresstionString[i + 1].integer * Array_ExpresstionString[i + 1].denominator + Array_ExpresstionString[i + 1].molecule) * (Array_ExpresstionString[i - 1].denominator));
+                let b = (Array_ExpresstionString[i - 1].denominator) * (Array_ExpresstionString[i + 1].denominator);
+
+                if (a <= 0) {
+                    return null;
+                } else {
+                    let newnumber = new True_Faction(a, b);
+                    Array_ExpresstionString.splice(i - 1, 3, newnumber);
+                }
+            }
+        }
+        let answer = Array_ExpresstionString[0];
+        return answer;
+    }
+
+    /*
+    测试区
+    */
+    // let xx = new True_Faction(2, 8);
+    // console.log(xx.toString());
+    // console.log(eval("3+9/2+4"));
+    let xx = new Four_Airthmetic(10, 10);
+    let expresstion = xx.getOneAirthmetic();
+    console.log(expresstion);
+    // xx.getOneAirthmetic();
+    // xx.getOneAirthmetic();
+    // getAnswerFromExpresstion();
+}
